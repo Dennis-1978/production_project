3a42d755b9a908532e8e6c58c7e671303108f667
Denis <d.khokhloff2011@yandex.ru>
2024-01-15 17:10:02 +0300

    decompose the webpack configuration, create options to manage the build

---
+++ config/build/buildPlugins.ts
@@ -0,0 +1,16 @@
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import webpack from "webpack";
+import { BuildOptions } from "./types/config";
+
+export function buildPlugins({
+	paths,
+}: BuildOptions): webpack.WebpackPluginInstance[] {
+
+	return [
+		new HtmlWebpackPlugin({
+			title: "Project",
+			template: paths.html,
+		}),
+		new webpack.ProgressPlugin(),
+	];
+}
