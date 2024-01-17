0a26308d74fdc6a210a59f299cff5794934b7c9b
Denis <d.khokhloff2011@yandex.ru>
2024-01-17 15:05:45 +0300

    to realise Suspense and Lazy

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
+import { Suspense } from "react";
 import { Routes, Route, Link } from "react-router-dom";
 
 import { Counter } from "./components/Counter";
-import MainPage from "./pages/MainPage/MainPage";
-import AboutPage from "./pages/AboutPage/AboutPage";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 
 import "./index.scss";
 
 const App = () => {
 	return (
 		<div className='app'>
-			<Link to={'/'}>Main</Link>
-			<Link to={'/about'}>About</Link>
-			<Routes>
-				<Route
-					path={"/"}
-					element={<MainPage />}
-				/>
-				<Route
-					path={"/about"}
-					element={<AboutPage />}
-				/>
-			</Routes>
+			<Link to={"/"}>Main</Link>
+			<Link to={"/about"}>About</Link>
+			<Suspense fallback={<div>Loading...</div>}>
+				<Routes>
+					<Route
+						path={"/"}
+						element={<MainPageAsync />}
+					/>
+					<Route
+						path={"/about"}
+						element={<AboutPageAsync />}
+					/>
+				</Routes>
+			</Suspense>
 			Hello styles
 			<Counter />
 		</div>
