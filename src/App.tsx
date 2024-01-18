d2a011662a984884b75d7e21b9581dac0631ccec
Denis <d.khokhloff2011@yandex.ru>
2024-01-18 15:34:06 +0300

    organisation of styles, add to implement switching of dark and light themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
-import { Suspense } from "react";
+import { Suspense, useContext } from "react";
 import { Routes, Route, Link } from "react-router-dom";
 
 import { Counter } from "./components/Counter";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import { Theme } from "./theme/ThemeContext";
+import { useTheme } from "./theme/useTheme";
 
-import "./index.scss";
+import "./styles/index.scss";
 
 const App = () => {
+	const { theme, toggleTheme } = useTheme();
+
 	return (
-		<div className='app'>
+		<div className={`app ${theme}`}>
 			<Link to={"/"}>Main</Link>
 			<Link to={"/about"}>About</Link>
+			<button onClick={toggleTheme}>
+				{theme === Theme.DARK ? Theme.LIGHT : Theme.DARK}
+			</button>
 			<Suspense fallback={<div>Loading...</div>}>
 				<Routes>
 					<Route
@@ -24,7 +31,6 @@ const App = () => {
 					/>
 				</Routes>
 			</Suspense>
-			Hello styles
 			<Counter />
 		</div>
 	);
